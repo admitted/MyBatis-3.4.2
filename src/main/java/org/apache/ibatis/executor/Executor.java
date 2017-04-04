ba246d1fc92146604fbc9ac6afa2e2a205a73104
admitted <876198439@qq.com>
2017-04-04 23:49:54 +0800

    执行器理解

--- src/main/java/org/apache/ibatis/executor/Executor.java
+++ src/main/java/org/apache/ibatis/executor/Executor.java
@@ -1,22 +1,19 @@
 /**
- *    Copyright 2009-2015 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2015 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.executor;
-
-import java.sql.SQLException;
-import java.util.List;
 
 import org.apache.ibatis.cache.CacheKey;
 import org.apache.ibatis.cursor.Cursor;
@@ -27,41 +24,45 @@ import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.transaction.Transaction;
 
+import java.sql.SQLException;
+import java.util.List;
+
 /**
+ * 执行器接口
  * @author Clinton Begin
  */
 public interface Executor {
 
-  ResultHandler NO_RESULT_HANDLER = null;
+    ResultHandler NO_RESULT_HANDLER = null;
 
-  int update(MappedStatement ms, Object parameter) throws SQLException;
+    int update(MappedStatement ms, Object parameter) throws SQLException;
 
-  <E> List<E> query(MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, CacheKey cacheKey, BoundSql boundSql) throws SQLException;
+    <E> List<E> query(MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, CacheKey cacheKey, BoundSql boundSql) throws SQLException;
 
-  <E> List<E> query(MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler) throws SQLException;
+    <E> List<E> query(MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler) throws SQLException;
 
-  <E> Cursor<E> queryCursor(MappedStatement ms, Object parameter, RowBounds rowBounds) throws SQLException;
+    <E> Cursor<E> queryCursor(MappedStatement ms, Object parameter, RowBounds rowBounds) throws SQLException;
 
-  List<BatchResult> flushStatements() throws SQLException;
+    List<BatchResult> flushStatements() throws SQLException;
 
-  void commit(boolean required) throws SQLException;
+    void commit(boolean required) throws SQLException;
 
-  void rollback(boolean required) throws SQLException;
+    void rollback(boolean required) throws SQLException;
 
-  CacheKey createCacheKey(MappedStatement ms, Object parameterObject, RowBounds rowBounds, BoundSql boundSql);
+    CacheKey createCacheKey(MappedStatement ms, Object parameterObject, RowBounds rowBounds, BoundSql boundSql);
 
-  boolean isCached(MappedStatement ms, CacheKey key);
+    boolean isCached(MappedStatement ms, CacheKey key);
 
-  void clearLocalCache();
+    void clearLocalCache();
 
-  void deferLoad(MappedStatement ms, MetaObject resultObject, String property, CacheKey key, Class<?> targetType);
+    void deferLoad(MappedStatement ms, MetaObject resultObject, String property, CacheKey key, Class<?> targetType);
 
-  Transaction getTransaction();
+    Transaction getTransaction();
 
-  void close(boolean forceRollback);
+    void close(boolean forceRollback);
 
-  boolean isClosed();
+    boolean isClosed();
 
-  void setExecutorWrapper(Executor executor);
+    void setExecutorWrapper(Executor executor);
 
 }
